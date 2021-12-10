8c7de2bfdf060858f4f20fe6c6871da95b41980a
Chris <medienmonster2013@gmail.com>
2021-12-10 15:49:20 +0100

    Keep dialog on rotation change

--- app/src/main/java/io/github/diduseetheocean/pixabayapp/ui/components/misc/SearchViewContent.kt
+++ app/src/main/java/io/github/diduseetheocean/pixabayapp/ui/components/misc/SearchViewContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -32,7 +33,7 @@ fun SearchViewContent(
     searchState: SearchState = Empty,
     onImageClicked: (Long) -> Unit = {},
 ) {
-    var showDialog by remember { mutableStateOf(false) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
     var imageId by remember { mutableStateOf(-1L) }
 
     Box(
